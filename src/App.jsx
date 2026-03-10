0985b242aa4dff9996bd7a4f5784f10f359ca593
Pyn Chaingin <stevie@Pyns-Macbook-Pro.local>
2026-03-10 20:05:06 +0700

    add router / fixed cookies

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react'
 import LoginScreen from './LoginScreen'
 import BookScreen from './BookScreen'
 import EditBook from './EditBook'
+import { Link, Navigate, Route, Routes, useNavigate } from 'react-router-dom'
 import Cookies from 'js-cookie'
 
 //This is router branch
@@ -13,24 +14,46 @@ axios.defaults.baseURL = "http://localhost:3000"
 function App() {
 
   const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const navigate = useNavigate()
 
-  useEffect(() => {
+  // useEffect(() => {
+  //   const token = Cookies.get('auth')
+  //   if (token != null) {
+  //     axios.defaults.headers.common = { "Authorization" : `bearer ${token}`}
+  //     setIsAuthenticated(true)
+  //   }
+  // }, [])
+
+  const ProtectRoute = ({children}) => {
     const token = Cookies.get('auth')
-    if (token != null) {
-      axios.defaults.headers.common = { "Authorization" : `bearer ${token}`}
-      setIsAuthenticated(true)
+    if (!token){
+      return <Navigate to='/login' replace />
     }
-  }, [])
+    axios.defaults.headers.common = {'Authorization' : `bearer ${token}`}
+
+    return children
+  }
   
 
   const handleLoginSuccess = (token) => {
     Cookies.set('auth', token)
     setIsAuthenticated(true)
+    navigate('/')
+
   }
 
   return (
     <>
-      {isAuthenticated ? <BookScreen /> : (<LoginScreen onLoginSuccess={handleLoginSuccess}></LoginScreen>)}
+      <Routes>
+        <Route path="/" element={
+          <ProtectRoute>
+            <BookScreen isAuthenticated={isAuthenticated} />
+          </ProtectRoute>
+          }></Route>
+        <Route path="/login" element={<LoginScreen onLoginSuccess={handleLoginSuccess} />}></Route>
+        <Route path="*" element={<div><h1>Not found</h1><Link to={'/'}>Home</Link></div>}/>
+      </Routes>
+
     </>
   )
 }
